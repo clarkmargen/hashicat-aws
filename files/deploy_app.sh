366582ddcc3f1342e125fbb9eb0c3d8a8aaa2bac
clarkmargen <79298868+clarkmargen@users.noreply.github.com>
2021-02-19 15:53:19 +0800

    Update deploy_app.sh

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -11,7 +11,7 @@ cat << EOM > /var/www/html/index.html
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
   <center><h2>PUGANTENG GUTOM RULES!!!!!</h2></center>
-  <center>welcome to ${PREFIX}'s app. Pagtumayo hindi kita paa</center>
+  <center>welcome to ${PREFIX}'s app. Pagtumayo hindi kita paa nya</center>
   <!-- END -->
 
   </div>
